0afd34986f004169a56d05c2cf9055481c5d6c07
ruo-yu-Pan <41214216+ruo-yu-Pan@users.noreply.github.com>
2020-07-14 20:36:24 +0800

    change_path_setting

--- Data_Information/Script_Plot_NorthSea_subarea_map.R
+++ Data_Information/Script_Plot_NorthSea_subarea_map.R
@@ -1,7 +1,9 @@
 windowsFonts(NR = "Times New Roman")
 
 #2019/06/18 Plotting
-setwd("E:/Master/Future_Research/Data/NorthSea/Analysis")
+setwd("D:\\Ruo_data\\2019_Paper_Publish\\Code\\Git\\Data")
+fig_path = "D:\\Ruo_data\\2019_Paper_Publish\\Figure\\"
+
 northsea <- read.csv("CPUE per length per subarea_2016-09-21_1965to2016_q1q3.csv", header = T, sep = ",")
 tot_sta<-as.data.frame(xtabs(~Subarea,data = northsea))
 
@@ -17,7 +19,6 @@ subarea_symb <- data.frame(subarea=tot_sta[,1],
 
 library(maps)
 library(mapdata)
-library(ggplot2)
 
 min_lat =min(subarea_symb$lat)
 max_lat =max(subarea_symb$lat)
@@ -32,12 +33,12 @@ grid_size_lon = 1
 lat_vec =  seq(min_lat-2.25,max_lat+2.25,grid_size_lat)
 lon_vec =  seq(min_lon-5.5,max_lon+5.5,grid_size_lon)
 
-jpeg("D:\\Ruo_data\\2019_Paper_Publish\\Figure\\NorthSea_subarea_map.jpeg", width=3.5, height=4.5, units = "in",res=300)
+jpeg(paste(fig_path,"NorthSea_subarea_map.jpeg",sep=""), width=7, height=9, units = "in",res=300)
 
 plot(c(min_lon,min_lat),xlim=c(min_lon-5,max_lon+5),
-     ylim=c(min_lat-2,max_lat+2),type="n",xlab = "Longtitude",ylab="Latitude",family = "NR")
+     ylim=c(min_lat-2,max_lat+2),type="n",xlab = "Longtitude",ylab="Latitude",family = "NR",cex.lab=2)
 
-points(subarea_symb$long[-c(1:6)],subarea_symb$lat[-c(1:6)],pch=15,col="grey",cex=1.2)
+points(subarea_symb$long[-c(1:6)],subarea_symb$lat[-c(1:6)],pch=15,col="grey",cex=2.5)
 abline(v=lon_vec)
 abline(h=lat_vec)
 
